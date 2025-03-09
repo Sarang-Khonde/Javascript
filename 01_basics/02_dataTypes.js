03944bf9aa393939281085c174a1f40443d4ac71
Sarang Khonde <sarangkhonde89@gmail.com>
2025-03-09 07:41:38 +0000

    Learned about JS datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+let name = "Sarang"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // output is undefined
+console.log(typeof null); // output is Object
