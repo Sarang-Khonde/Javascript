a8181fede9e04656ee5959855d906fd5b43460c7
Sarang Khonde <sarangkhonde89@gmail.com>
2025-03-09 08:05:22 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+let score = "sarang";
+
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score);
+
+//console.log(typeof (valueInNumber));
+
+//console.log(valueInNumber);
+
+// "22" => 22
+// "22abc" => NaN (Not a number)
+// ture => 1 ; false => 0
+
+let isLoggedIn = "sarang";
+
+booleanIsLoggedIn = Boolean(isLoggedIn);
+
+//console.log( booleanIsLoggedIn);
+
+// 1 => true
+// "" => false
+// "sarang" => true
+
+let anyNumber = 43;
+
+let stringNumber = String(anyNumber);
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
