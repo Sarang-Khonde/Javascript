a6b5a4bc7b9a7bf40908d3e539f546de979fdd6f
Sarang Khonde <sarangkhonde89@gmail.com>
2025-03-09 08:38:14 +0000

    Gain little bit knowledge about conversion and operations in JS

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,12 +1,12 @@
 let score = "sarang";
 
-//console.log(typeof (score));
+console.log(typeof (score));
 
-//let valueInNumber = Number(score);
+let valueInNumber = Number(score);
 
-//console.log(typeof (valueInNumber));
+console.log(typeof (valueInNumber));
 
-//console.log(valueInNumber);
+console.log(valueInNumber);
 
 // "22" => 22
 // "22abc" => NaN (Not a number)
@@ -16,7 +16,7 @@ let isLoggedIn = "sarang";
 
 booleanIsLoggedIn = Boolean(isLoggedIn);
 
-//console.log( booleanIsLoggedIn);
+console.log( booleanIsLoggedIn);
 
 // 1 => true
 // "" => false
@@ -29,3 +29,34 @@ let stringNumber = String(anyNumber);
 console.log(stringNumber);
 
 console.log(typeof stringNumber);
+
+// ########################################### OPERATIONS ############################################
+
+let value = 5;
+let negValue = -value;
+console.log(negValue);
+console.log(typeof negValue);
+
+let str1 = "hello"
+let str2 = " sarang"
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log(1 + 2 + "2");
+
+console.log(true);
+
+
+// Prefix and postfix increment operator
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
